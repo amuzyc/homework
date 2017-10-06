bc45fc3facf5649a9158eaad2f09dc6dd16b9139
amuzyc <31899650+amuzyc@users.noreply.github.com>
2017-10-05 22:02:08 -0700

    Created a map class

--- W1D5/diy_adt.rb
+++ W1D5/diy_adt.rb
@@ -35,3 +35,34 @@ class Queue
     @queue.dup
   end
 end
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    p_index = @map.index {|pair| pair[0] == key}
+    p_index ? @map[p_index][1] = value : @map.push([key, value])
+    [key, value]
+  end
+
+  def lookup(key)
+    @map.each {|pair| return pair[1] if pair[0] == key}
+    nil
+  end
+
+  def remove(key)
+    @map.reject! {|pair| pair[0] == key}
+    nil
+  end
+
+  def show
+    deep_dup(@map)
+  end
+
+  private
+  def deep_dup(arr)
+    arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
+  end
+end
